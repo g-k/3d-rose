10ba4b5950f8c4395d87a28a4f50169ec2a0f883
Greg Guthe <gregg@aweber.com>
2013-02-09 19:32:20 -0500

    expand Math functions

--- 3d-rose.js
+++ 3d-rose.js
@@ -1,5 +1,9 @@
 // http://www.romancortes.com/
-with(m = Math) C = cos, S = sin, P = pow;
+m = Math;
+cos = Math.cos;
+sin = Math.sin;
+pow = Math.pow;
+
 O = a.getImageData(0, 0, c.height = f = W = 256, f); 
 U = O.data; // Array of zeroes 256 (height) * 256 (width) * 4 (RGBA) = 262144 (or 2^18)
 D = {};
@@ -13,6 +17,7 @@ function J(p) {
       }
     }
 }
+
 var animate = function () {
     for (i = 1e3; i--;) {
         c = i % 42 * 1.35;
@@ -20,13 +25,13 @@ var animate = function () {
         T = m.random();
         A = H * 2 - 1;
         B = T * 2 - 1;
-        J([S(H * 7) * (o = 13 + 5 / (.2 + P(T * 4, 4))) - T * 50, T * 550 + 500, (l = C(H * 7)) * o, (G = l / 7 + .5) - T / 4, G]);
+        J([sin(H * 7) * (o = 13 + 5 / (.2 + pow(T * 4, 4))) - T * 50, T * 550 + 500, (l = cos(H * 7)) * o, (G = l / 7 + .5) - T / 4, G]);
         if (A * A + B * B < 1) {
           if (c > 32) {
-              J([(o = .5 / (H + .01) - H * 300) * C(n = (j = c & 1) ? 6 : 4) + (w = T * -f) * S(n) + j * 630 - 390, o * S(n) - w * C(n) + 999 - j * 350, C(B + A) * 99 - j * 50, (P(l = 1 - B * B, f * 6) + C(H + T) + P(C((o * H + o + (B > 0 ? w : -w)) / 25), 30) * l - H + 2) / 5, o / 1e3 + .7 - o * w / 3e5]);
-              J([(o = H * 45 - 20) * C(l = c / .86) + (w = T * T) * f * S(l), C(B / 2) * 99 - w * T * 60 + 436, o * S(l) - w * f * C(l), w * .3 + .3, T * .7]);
+              J([(o = .5 / (H + .01) - H * 300) * cos(n = (j = c & 1) ? 6 : 4) + (w = T * -f) * sin(n) + j * 630 - 390, o * sin(n) - w * cos(n) + 999 - j * 350, cos(B + A) * 99 - j * 50, (pow(l = 1 - B * B, f * 6) + cos(H + T) + pow(cos((o * H + o + (B > 0 ? w : -w)) / 25), 30) * l - H + 2) / 5, o / 1e3 + .7 - o * w / 3e5]);
+              J([(o = H * 45 - 20) * cos(l = c / .86) + (w = T * T) * f * sin(l), cos(B / 2) * 99 - w * T * 60 + 436, o * sin(l) - w * f * cos(l), w * .3 + .3, T * .7]);
           } else { 
-            J([(o = A * (2 - T) * (80 - c * 2)) * C(c) - (w = 99 - C(A) * 120 - C(T) * (f - c * 5) + C(P(1 - T, 7)) * 50 + c * 2) * S(c), (B * 2 - C(P(T, 7)) + 9) * 50, o * S(c) + w * C(c), 1 - T * .7, P(1 - T, 9) / 4]);
+            J([(o = A * (2 - T) * (80 - c * 2)) * cos(c) - (w = 99 - cos(A) * 120 - cos(T) * (f - c * 5) + cos(pow(1 - T, 7)) * 50 + c * 2) * sin(c), (B * 2 - cos(pow(T, 7)) + 9) * 50, o * sin(c) + w * cos(c), 1 - T * .7, pow(1 - T, 9) / 4]);
           }
         }
     }
@@ -35,7 +40,7 @@ var animate = function () {
         U[i * 4 + l] = 255;
       }
     }
-    for (c = C(W), s = S(W), j = F.length; j--;) {
+    for (c = cos(W), s = sin(W), j = F.length; j--;) {
         var k = F[j];
         var x = k[0] * c + k[2] * s + 99;
         var z = k[0] * s - k[2] * c; 
